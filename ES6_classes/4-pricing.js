76b56f3f6536efe34d43928fbd0abca59e657035
Agilda Kulli <agildakulli@gmail.com>
2026-01-22 17:40:04 +0100

    task 4 e rregulluar

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,39 +1,40 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
-export default class Pricing{
-    constructor (amount , currency){
-        if (typeof amount !== 'number'){
-            throw new TypeError('Amount must be a number');
-        }
-        if (!(currency instanceof Currency)){
-            throw new Error ('Currency must be a currency type ');
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be a currency type');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-        }
-        this._amount = amount;
-        this._currency = currency;
-    }
-    get amount(){
-        return this._amount;
-    }
-    set amount(amount ){
-        if (typeof amount !== 'number'){
-            throw new Error('Amount must be a number');
-        }
-        this._amount = amount;
-    }
-    set currency(currency){
-        if (!(currency instanceof Currency)){
-            throw new Error ('Currency must be a currency type ');
-        }
-        this._currency = currency;
-    
-    }
-    displayFullPrice(){
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
-    static convertPrice(amount, conversionRate ){
-        return amount * conversionRate;
-    }
+  get amount() {
+    return this._amount;
+  }
 
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    this._amount = amount;
+  }
 
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be a currency type');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} (${this._currency.name}) (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
